828091378db2f0d8a954ebc011239355a45b9809
Jacob Hakim <hakimj@hawaii.edu>
2021-01-06 11:16:10 -1000

    commit changes week 02 Tidy Tuesday

--- R_scripts/transit-costs/transit-costs.R
+++ R_scripts/transit-costs/transit-costs.R
@@ -1,9 +1,10 @@
-## Tidy Tuesday - Black Firsts
+## Tidy Tuesday - Transit Costs (Tokyo)
 
 # Load libraries
 library(tidyverse)
 library(extrafont)
 library(ggtext)
+library(forcats)
 
 # Get the data
 transit_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-05/transit_cost.csv')
@@ -11,154 +12,54 @@ transit_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascien
 # set working directory
 setwd("C:/Users/jake/Documents/R/tidy-tuesday/TidyTuesday_Hakim/R_scripts/transit-costs/")
 
-female_firsts <- firsts %>%  
-  mutate(decade = year - year %% 10) %>% # add decade values
-  filter(gender=="Female African American Firsts") %>%
-  filter(!(year == 1760)) %>% # Remove Jupiter Hammon (error)
-  group_by(decade) %>%
-  arrange(category) %>% # arrange categories for bar graph
-  mutate(category = recode(category,
-                           "Social & Jobs" = "Social\n& Jobs",
-                           "Education & Science" = "Education\n& Science")) %>%
-  mutate(index = row_number()) %>%
-  ungroup()
+tokyo <- transit_cost %>% 
+  filter(city == "Tokyo") %>%
+  mutate(real_cost=as.numeric(real_cost)) %>%
+  mutate(trillion=real_cost/1000) %>%
+  arrange(-desc(start_year)) %>%
+  mutate(line=as.factor(line))
   
-
-# BAR PLOT
-ggplot(female_firsts, aes(x=decade, y=1, group = index, fill=category)) +
   
-  geom_bar(stat="identity",
-           size=1.1,
-           width=10,
-           color="#FFF5EE") +
+# POLAR BAR PLOT
+ggplot(tokyo, aes(x=fct_rev(fct_reorder(line, start_year)), y=length, fill=trillion)) +
+  geom_col(width=0.3) +
+  coord_flip() +
   
   # color and key stuff
-  scale_fill_brewer(palette = "Paired",
-                    guide = guide_legend(direction = "horizontal", nrow = 2)) +
+  scale_fill_gradient(name="Trillions USD",
+                      guide=guide_legend(direction="horizontal"),
+                      low="#fb6a4a",
+                      high="#a50f15",
+                      aesthetics="fill") +
 
-  # x axis labels
-  scale_x_continuous(name = "Decade",
-                   breaks = seq(1770, 2010, by = 10),
-                   labels = paste0(seq(1770, 2010, by = 10), "s"),
-                   expand = c(0, 1)) +
   
- # title
-   annotate(geom = "text",
-           x=1800,y=20,
-           hjust=0.14, vjust=0.5,
-           label="Achievements of Black Women in America",
-           size=13, lineheight=.8,
-           family="Impact",
-           color="#2F4F4F") +
+  labs(title="Railways of Tokyo", caption="Data: Transit Costs Project | Visualization: @JacobHakim2") +
   
-  #add line and label for 13th amendment
-  geom_segment(x=1860, y=0, xend=1860, yend=15, #1865 didn't line up right
-             linetype="dotted") +
-  
-  annotate(geom = "text",
-           x=1859,y=5.3,
-           hjust=0, vjust=0,
-           label="December 18, 1865: \nAdoption of the 13th Amendment",
-           size=3.5, lineheight=.8,
-           color="#708090",
-           angle=90) +
-  
-  # add line and label for 19th amendment
-  geom_segment(x=1916, y=0, xend=1916, yend=15, # 1920 didn't line up right
-               linetype="dotted") +
-  
-  annotate(geom = "text",
-           x=1915,y=5.3,
-           hjust=0, vjust=0,
-           label="August 26, 1920: \nAdoption of the 19th Amendment",
-           size=3.5, lineheight=.8,
-           color="#708090",
-           angle=90) +
-  
-  labs(caption="Data: Wikipedia | Visualization: @JacobHakim2") +
+  ylab("Length (km)") +
   
   # theme stuff
-  theme(plot.background = element_rect(fill="#FFF5EE", color = "#FFF5EE"),
-        panel.background = element_rect(fill="#FFF5EE", color = "#FFF5EE"),
+  theme(plot.background = element_rect(fill="white", color = "white"),
+        panel.background = element_rect(fill="white", color = "white"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
+        axis.text.y = element_text(size=9,
+                                   family="Palatino Linotype"),
         axis.ticks = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text.x = element_text(hjust=2, vjust=3,
-                                   face="bold",
-                                   size=8, angle=50),
-        legend.key.size = unit(0.8, "cm"),
-        legend.key.width = unit(1,"cm"),
-        legend.position = c(0.3, 0.8),
-        legend.text = element_text(size = 10),
-        legend.title = element_blank(),
-        legend.background = element_rect(fill="#FFF5EE", color = "#FFF5EE"),
+        axis.title.x = element_text(size=9),
+        axis.text.x = element_text(size=8,
+                                   family="Palatino Linotype"),
+        legend.position = c(0.75, 1),
+        legend.text = element_text(size = 8),
+        legend.title = element_text(size=8),
+        legend.background = element_rect(fill="white", color = "white"),
+        plot.title = element_text(color="black",
+                                  size=30,
+                                  hjust=-1.6,
+                                  family="Palatino Linotype"),
         plot.caption = element_text(color="#A9A9A9",
-                                    family="Verdana")) +
+                                    family="Palatino Linotype")) 
 
-  # add line and label for achievements
-  # Phillis Wheatley
-  geom_curve(x = 1770, y = 9, 
-             xend = 1769, yend = 1,
-             curvature = 0.1,
-             size=0.6) +
-
-  annotate(geom="text",
-           x = 1765, y = 10.5,
-           hjust=0,
-           label="Phillis Wheatley",
-           size=4, lineheight=.8,
-           fontface="bold",
-           color="#708090") +
-  annotate(geom="text",
-           x = 1765, y = 9.85,
-           hjust=-0.02,
-           label="First African American Woman\nTo Publish a Book, 1773",
-           size=2.5, lineheight=.8,
-           color="#708090") +
-
-# Dr. Rebecca Davis Lee Crumpler
-geom_curve(x = 1870, y = 8.9, 
-           xend = 1865.5, yend = 4.5,
-           curvature = -0.1,
-           size=0.6) +
   
-  annotate(geom="text",
-           x = 1865, y = 10.5,
-           hjust=0,
-           label="Dr. Rebecca Davis \nLee Crumpler",
-           size=4, lineheight=.8,
-           fontface="bold",
-           color="#708090") +
-  annotate(geom="text",
-           x = 1865, y = 9.6,
-           hjust=-0.02,
-           label="First African American \nWoman to Earn An M.D., 1864",
-           size=2.5, lineheight=.8,
-           color="#708090") +
-
-
-# Condoleezza Rice
-geom_curve(x = 1955, y = 13, 
-           xend = 1995, yend = 11.4,
-           curvature = 0.1,
-           size=0.6) +
-  
-  annotate(geom="text",
-           x = 1925, y = 14.3,
-           hjust=0,
-           label="Condoleezza Rice",
-           size=4, lineheight=.8,
-           fontface="bold",
-           color="#708090") +
-  annotate(geom="text",
-           x = 1925, y = 13.5,
-           hjust=-0.02,
-           label="First African American Woman \nto Become Secretary of State,\n 2005",
-           size=2.5, lineheight=.8,
-           color="#708090")
-
 # save as jpg (9.57 x 6.92)
-ggsave("female-african-american-achievements.jpg", width=9.57, height=6.92, dpi=600)  
+ggsave("transit-costs-tokyo.jpg", dpi=600)  
